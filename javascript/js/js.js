7d0be577a8b15ceb0059a3fbab533c77ff0954f0
codemasterram <codemaster.ram@gmail.com>
2017-05-20 11:21:14 +0545

    nirose lae haldeko folder maule hereko

---
+++ javascript/js/js.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', handelLoad);
+
+function handelLoad(){
+	var main = document.querySelector('.mainImage');
+	var sub1 = document.querySelector('.subImage1');
+	var sub2 = document.querySelector('.subImage2');
+	var sub3 = document.querySelector('.subImage3');
+	var sub4 = document.querySelector('.subImage4');
+
+
+	if (main) {
+		console.log('Main Image Is clescked');
+	}
+	if (sub1) {
+		console.log('sub1 Image Is clescked');
+	}
+	if (sub2) {
+		console.log('sub2 Image Is clescked');
+	}
+	if (sub3) {
+		console.log('sub3 Image Is clescked');
+	}
+	if (sub4) {
+		console.log('sub4 Image Is clescked');
+	}
+
+
+}
